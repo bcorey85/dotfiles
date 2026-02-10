69e500f68c1e64dd61d8a851a08aa2dd7a9efa94
Brandon Corey <bcorey85@gmail.com>
2026-02-09 22:32:00 -0600

    fix git signs bind

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -7,4 +7,9 @@ return {
       virt_text_pos = "eol",
     },
   },
+  keys = {
+    { "<leader>ghd", function() require("gitsigns").diff_this() end, desc = "Diff This (Hunk)" },
+    -- clear LazyVim's default <leader>gd so diffview owns it
+    { "<leader>gd", false },
+  },
 }

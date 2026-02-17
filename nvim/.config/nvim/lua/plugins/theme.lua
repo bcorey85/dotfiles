43d10533629ce6e53b2956a796b5d15a0acc3d2f
Brandon Corey <bcorey85@gmail.com>
2026-02-17 11:26:29 -0600

    update theme

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,13 +1,31 @@
+-- return {
+--   {
+--     "sainnhe/sonokai",
+--     lazy = false,
+--     priority = 1000,
+--     config = function()
+--       vim.g.sonokai_style = "maia"
+--       vim.cmd.colorscheme("sonokai")
+--       vim.api.nvim_set_hl(0, "Directory", { fg = "#e3e1e4" })
+--       vim.api.nvim_set_hl(0, "MiniIconsAzure", { fg = "#9ecd6f" })
+--     end,
+--   },
+-- }
 return {
   {
-    "sainnhe/sonokai",
+    "everviolet/nvim",
+    name = "evergarden",
     lazy = false,
     priority = 1000,
-    config = function()
-      vim.g.sonokai_style = "maia"
-      vim.cmd.colorscheme("sonokai")
-      vim.api.nvim_set_hl(0, "Directory", { fg = "#e3e1e4" })
-      vim.api.nvim_set_hl(0, "MiniIconsAzure", { fg = "#9ecd6f" })
+    config = function(_, opts)
+      require("evergarden").setup(opts)
+      vim.cmd.colorscheme("evergarden")
     end,
+    opts = {
+      theme = {
+        variant = "fall",
+        accent = "green",
+      },
+    },
   },
 }

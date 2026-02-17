790f6ce5cd390a686548340f1301dab0b6cd20a9
Brandon Corey <bcorey85@gmail.com>
2026-02-17 10:58:32 -0600

    update obsidian, add revise command

--- nvim/.config/nvim/lua/plugins/obsidian.lua
+++ nvim/.config/nvim/lua/plugins/obsidian.lua
@@ -36,7 +36,7 @@ return {
           function()
             ensure_editable_win()
             vim.cmd("enew")
-            vim.api.nvim_feedkeys(":ObsidianNew ", "n", false)
+            vim.api.nvim_feedkeys(":Obsidian new ", "n", false)
           end,
           desc = "New note",
         },
@@ -44,15 +44,15 @@ return {
           "<leader>oN",
           function()
             ensure_editable_win()
-            vim.cmd("ObsidianNewFromTemplate")
+            vim.cmd("Obsidian new_from_template")
           end,
           desc = "New from template",
         },
-        { "<leader>oo", "<cmd>ObsidianQuickSwitch<cr>", desc = "Quick switch" },
-        { "<leader>ot", "<cmd>ObsidianTemplate<cr>", desc = "Insert template" },
-        { "<leader>ob", "<cmd>ObsidianBacklinks<cr>", desc = "Backlinks" },
-        { "<leader>os", "<cmd>ObsidianSearch<cr>", desc = "Search vault" },
-        { "<leader>of", "<cmd>ObsidianFollowLink<cr>", desc = "Follow link" },
+        { "<leader>oo", "<cmd>Obsidian quick_switch<cr>", desc = "Quick switch" },
+        { "<leader>ot", "<cmd>Obsidian template<cr>", desc = "Insert template" },
+        { "<leader>ob", "<cmd>Obsidian backlinks<cr>", desc = "Backlinks" },
+        { "<leader>os", "<cmd>Obsidian search<cr>", desc = "Search vault" },
+        { "<leader>of", "<cmd>Obsidian follow_link<cr>", desc = "Follow link" },
         { "<leader>om", desc = "Move note to folder" },
       }
     end)(),
@@ -60,46 +60,56 @@ return {
       require("obsidian").setup(opts)
 
       vim.keymap.set("n", "<leader>om", function()
-        local vault = vim.fn.finddir(".obsidian", vim.fn.expand("%:p:h") .. ";")
-        if vault == "" then
-          vim.notify("Not in an Obsidian vault", vim.log.levels.WARN)
-          return
-        end
-        local vault_root = vim.fn.fnamemodify(vault, ":h")
+        local client = require("obsidian").get_client()
+        local vault_root = tostring(client.dir)
 
-        vim.ui.select({
-          "00. Inbox",
-          "01. Literature",
-          "02. Permanent",
-          "90. Projects",
-          "91. Areas",
-          "92. Resources",
-          "93. Archives",
-          "Files",
-        }, { prompt = "Move note to:" }, function(choice)
-          if choice then
-            local src = vim.fn.expand("%:p")
-            local fname = vim.fn.expand("%:t")
-            local dest = vault_root .. "/" .. choice .. "/" .. fname
-            local old_buf = vim.api.nvim_get_current_buf()
-
-            vim.cmd("write")
-            vim.fn.rename(src, dest)
-            vim.cmd("edit " .. vim.fn.fnameescape(dest))
-            vim.api.nvim_buf_delete(old_buf, { force = true })
-            vim.notify("Moved to " .. choice, vim.log.levels.INFO)
+        local dirs = {}
+        local function scan(dir)
+          local entries = vim.fn.readdir(dir)
+          for _, name in ipairs(entries) do
+            if name:sub(1, 1) ~= "." and name ~= "Templates" then
+              local full = dir .. "/" .. name
+              if vim.fn.isdirectory(full) == 1 then
+                local rel = full:sub(#vault_root + 2)
+                table.insert(dirs, rel)
+                scan(full)
+              end
+            end
           end
-        end)
+        end
+        scan(vault_root)
+        table.sort(dirs)
+
+        require("fzf-lua").fzf_exec(dirs, {
+          prompt = "Move note to> ",
+          actions = {
+            ["default"] = function(selected)
+              if not selected or #selected == 0 then return end
+              local choice = selected[1]
+              local src = vim.fn.expand("%:p")
+              local fname = vim.fn.expand("%:t")
+              local dest = vault_root .. "/" .. choice .. "/" .. fname
+              local old_buf = vim.api.nvim_get_current_buf()
+
+              vim.cmd("write")
+              vim.fn.rename(src, dest)
+              vim.cmd("edit " .. vim.fn.fnameescape(dest))
+              vim.api.nvim_buf_delete(old_buf, { force = true })
+              vim.notify("Moved to " .. choice, vim.log.levels.INFO)
+            end,
+          },
+        })
       end, { desc = "Move note to folder" })
     end,
     opts = {
+      legacy_commands = false,
       picker = {
         name = "fzf-lua",
       },
       workspaces = {
         {
           name = "general",
-          path = vim.fn.expand("~/vault"),
+          path = vim.fn.expand("~/vault/general"),
         },
       },
       templates = {

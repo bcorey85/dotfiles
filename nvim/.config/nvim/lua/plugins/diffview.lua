ac586afff54dd6d85a489d10be288b1afddef7a4
Brandon Corey <bcorey85@gmail.com>
2026-02-18 09:50:08 -0600

    diffview and theme update

--- nvim/.config/nvim/lua/plugins/diffview.lua
+++ nvim/.config/nvim/lua/plugins/diffview.lua
@@ -15,7 +15,12 @@ return {
         local lib = require("diffview.lib")
         if lib.get_current_view() then
           vim.cmd("DiffviewClose")
+          local zoomed = vim.fn.system("tmux display-message -p '#{window_zoomed_flag}'"):gsub("%s+", "")
+          if zoomed == "1" then
+            vim.fn.system("tmux resize-pane -Z")
+          end
         else
+          vim.fn.system("tmux resize-pane -Z")
           vim.cmd("DiffviewOpen")
         end
       end,
@@ -27,7 +32,12 @@ return {
         local lib = require("diffview.lib")
         if lib.get_current_view() then
           vim.cmd("DiffviewClose")
+          local zoomed = vim.fn.system("tmux display-message -p '#{window_zoomed_flag}'"):gsub("%s+", "")
+          if zoomed == "1" then
+            vim.fn.system("tmux resize-pane -Z")
+          end
         else
+          vim.fn.system("tmux resize-pane -Z")
           vim.cmd("DiffviewFileHistory %")
         end
       end,

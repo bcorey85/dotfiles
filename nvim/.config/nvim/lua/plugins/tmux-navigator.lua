39e172de77564a8884584774a0e6de46da0eaf23
Brandon Corey <bcorey85@gmail.com>
2026-02-25 08:56:42 -0600

    disable vim tmux nav

--- nvim/.config/nvim/lua/plugins/tmux-navigator.lua
+++ nvim/.config/nvim/lua/plugins/tmux-navigator.lua
@@ -1,15 +1,17 @@
-return {
-  "christoomey/vim-tmux-navigator",
-  cmd = {
-    "TmuxNavigateLeft",
-    "TmuxNavigateDown",
-    "TmuxNavigateUp",
-    "TmuxNavigateRight",
-  },
-  keys = {
-    { "<C-h>", "<cmd>TmuxNavigateLeft<cr>" },
-    { "<C-j>", "<cmd>TmuxNavigateDown<cr>" },
-    { "<C-k>", "<cmd>TmuxNavigateUp<cr>" },
-    { "<C-l>", "<cmd>TmuxNavigateRight<cr>" },
-  },
-}
+-- temporarily disabled — uncomment to re-enable
+-- return {
+--   "christoomey/vim-tmux-navigator",
+--   cmd = {
+--     "TmuxNavigateLeft",
+--     "TmuxNavigateDown",
+--     "TmuxNavigateUp",
+--     "TmuxNavigateRight",
+--   },
+--   keys = {
+--     { "<C-h>", "<cmd>TmuxNavigateLeft<cr>" },
+--     { "<C-j>", "<cmd>TmuxNavigateDown<cr>" },
+--     { "<C-k>", "<cmd>TmuxNavigateUp<cr>" },
+--     { "<C-l>", "<cmd>TmuxNavigateRight<cr>" },
+--   },
+-- }
+return {}

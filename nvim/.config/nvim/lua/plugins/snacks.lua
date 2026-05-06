e62b8dc011eb1ce79b2360903d905b19744b3ce4
Brandon Corey <bcorey85@gmail.com>
2026-05-05 21:09:43 -0500

    update snacks and lazygit command

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -57,5 +57,32 @@ return {
       end,
       desc = "Git Status (sidebar)",
     },
+    {
+      "<leader>gg",
+      function()
+        local in_tmux = vim.env.TMUX ~= nil
+        if in_tmux then
+          local zoomed = vim.fn.system("tmux display-message -p '#{window_zoomed_flag}'"):gsub("%s+", "")
+          if zoomed ~= "1" then
+            vim.fn.system("tmux resize-pane -Z")
+          end
+        end
+        Snacks.lazygit({
+          win = {
+            width = 0.99,
+            height = 0.99,
+            on_close = function()
+              if in_tmux then
+                local zoomed = vim.fn.system("tmux display-message -p '#{window_zoomed_flag}'"):gsub("%s+", "")
+                if zoomed == "1" then
+                  vim.fn.system("tmux resize-pane -Z")
+                end
+              end
+            end,
+          },
+        })
+      end,
+      desc = "Lazygit (zoomed)",
+    },
   },
 }

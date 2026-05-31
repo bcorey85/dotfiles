bc80f63404d189a909bfa3ead91aa95087c96f21
Brandon Corey <bcorey85@gmail.com>
2026-05-30 23:22:06 -0500

    octo and gs updates

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -10,8 +10,8 @@ return {
     signs = {
       add = { text = "▎" },
       change = { text = "▎" },
-      delete = { text = "" },
-      topdelete = { text = "" },
+      delete = { text = "_" },
+      topdelete = { text = "‾" },
       changedelete = { text = "▎" },
     },
     on_attach = function(bufnr)
@@ -22,15 +22,19 @@ return {
 
       map("n", "]h", function() gs.nav_hunk("next") end, "Next hunk")
       map("n", "[h", function() gs.nav_hunk("prev") end, "Prev hunk")
-      map("n", "<leader>ghs", gs.stage_hunk, "Stage hunk")
-      map("n", "<leader>ghr", gs.reset_hunk, "Reset hunk")
-      map("v", "<leader>ghs", function() gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") }) end, "Stage hunk")
-      map("v", "<leader>ghr", function() gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") }) end, "Reset hunk")
-      map("n", "<leader>ghS", gs.stage_buffer, "Stage buffer")
-      map("n", "<leader>ghR", gs.reset_buffer, "Reset buffer")
-      map("n", "<leader>ghp", gs.preview_hunk, "Preview hunk")
-      map("n", "<leader>ghb", function() gs.blame_line({ full = true }) end, "Blame line")
-      map("n", "<leader>ghd", function() gs.diffthis() end, "Diff this (hunk)")
+      map("n", "<leader>hs", gs.stage_hunk, "Stage hunk")
+      map("n", "<leader>hr", gs.reset_hunk, "Reset hunk")
+      map("v", "<leader>hs", function() gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") }) end, "Stage hunk")
+      map("v", "<leader>hr", function() gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") }) end, "Reset hunk")
+      map("n", "<leader>hS", gs.stage_buffer, "Stage buffer")
+      map("n", "<leader>hR", gs.reset_buffer, "Reset buffer")
+      map("n", "<leader>hp", gs.preview_hunk, "Preview hunk")
+      map("n", "<leader>hb", function() gs.blame_line({ full = true }) end, "Blame line")
+      map("n", "<leader>hd", function()
+        gs.diffthis()
+        vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = 0, silent = true })
+      end, "Diff this (hunk)")
+      map("n", "<leader>hD", gs.toggle_deleted, "Toggle deleted lines (inline)")
       map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "Select hunk")
     end,
   },
--- nvim/.config/nvim/lua/plugins/octo.lua
+++ nvim/.config/nvim/lua/plugins/octo.lua
@@ -29,15 +29,52 @@ return {
     suppress_missing_scope = {
       projects_v2 = true,
     },
+    mappings = {
+      review_diff = {
+        toggle_viewed = { lhs = "<leader>gv", desc = "toggle viewed" },
+        select_next_unviewed_entry = { lhs = "]r", desc = "next unviewed file" },
+        select_prev_unviewed_entry = { lhs = "[r", desc = "prev unviewed file" },
+      },
+      file_panel = {
+        toggle_viewed = { lhs = "<leader>gv", desc = "toggle viewed" },
+        select_next_unviewed_entry = { lhs = "]r", desc = "next unviewed file" },
+        select_prev_unviewed_entry = { lhs = "[r", desc = "prev unviewed file" },
+      },
+    },
   },
   keys = {
-    { "<leader>go", "", desc = "+octo (GitHub)" },
-    { "<leader>goc", "<cmd>Octo pr create<cr>", desc = "Create PR (Octo)" },
-    { "<leader>gop", "<cmd>Octo pr list<cr>", desc = "List PRs (Octo)" },
-    { "<leader>goP", octo_prompt("Octo PR search: ", "Octo pr search"), desc = "Search PRs (Octo)" },
-    { "<leader>goi", "<cmd>Octo issue list<cr>", desc = "List Issues (Octo)" },
-    { "<leader>goI", octo_prompt("Octo issue search: ", "Octo issue search"), desc = "Search Issues (Octo)" },
-    { "<leader>gor", octo_prompt("Repos for (blank = you): ", "Octo repo list", true), desc = "List Repos (Octo)" },
-    { "<leader>gos", octo_prompt("Octo search: ", "Octo search"), desc = "Search (Octo)" },
+    { "<leader>gh", "", desc = "+github (Octo)" },
+    {
+      "<leader>ghv",
+      function()
+        local url = vim.fn.getreg("+"):gsub("%s+", "")
+        if not url:match("^https?://.*github") then
+          Snacks.notify.warn("Clipboard doesn't look like a GitHub URL: " .. url)
+          return
+        end
+        vim.cmd("Octo " .. url)
+      end,
+      desc = "Open URL from clipboard (Octo)",
+    },
+    { "<leader>ghc", "<cmd>Octo pr create<cr>", desc = "Create PR (Octo)" },
+    { "<leader>ghp", "<cmd>Octo pr list<cr>", desc = "List PRs (Octo)" },
+    { "<leader>ghP", octo_prompt("Octo PR search: ", "Octo pr search"), desc = "Search PRs (Octo)" },
+    { "<leader>ghi", "<cmd>Octo issue list<cr>", desc = "List Issues (Octo)" },
+    { "<leader>ghI", octo_prompt("Octo issue search: ", "Octo issue search"), desc = "Search Issues (Octo)" },
+    { "<leader>ghr", octo_prompt("Repos for (blank = you): ", "Octo repo list", true), desc = "List Repos (Octo)" },
+    { "<leader>ghs", octo_prompt("Octo search: ", "Octo search"), desc = "Search (Octo)" },
+
+    -- Review / comments / threads (all under <leader>gr*)
+    { "<leader>gr", "", desc = "+review (Octo)" },
+    { "<leader>grs", "<cmd>Octo review start<cr>", desc = "Review: start" },
+    { "<leader>grc", "<cmd>Octo review resume<cr>", desc = "Review: continue" },
+    { "<leader>grx", "<cmd>Octo review submit<cr>", desc = "Review: submit" },
+    { "<leader>grd", "<cmd>Octo review discard<cr>", desc = "Review: discard" },
+    { "<leader>grp", "<cmd>Octo review comments<cr>", desc = "Review: pending comments" },
+    { "<leader>gra", "<cmd>Octo comment add<cr>", mode = { "n", "v" }, desc = "Comment: add" },
+    { "<leader>gre", "<cmd>Octo comment edit<cr>", desc = "Comment: edit" },
+    { "<leader>grk", "<cmd>Octo comment delete<cr>", desc = "Comment: kill (delete)" },
+    { "<leader>grt", "<cmd>Octo thread resolve<cr>", desc = "Thread: resolve" },
+    { "<leader>gru", "<cmd>Octo thread unresolve<cr>", desc = "Thread: unresolve" },
   },
 }
